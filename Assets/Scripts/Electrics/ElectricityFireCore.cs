3e56e67dd4979b5d5f8fdb597603d515369f3ac7
ManhTien <nguyentienmanh692004@gmail.com>
2026-04-14 22:47:37 +0700

    - update scene electricity

---
+++ Assets/Scripts/Electrics/ElectricityFireCore.cs
@@ -0,0 +1,28 @@
+using Core;
+using UnityEngine;
+
+namespace Electrics
+{
+    public class ElectricityFireCore : FireBase
+    {
+        public override void ProcessInteraction(GameObject tool)
+        {
+            // if (tool.layer.Equals("Lid"))
+            // {
+            //     Debug.Log("---Manh--- Check: Fire is interacting with lid");
+            //     FinishTraining(true, "You extinguished the oil fire correctly using the pot lid!");
+            // }
+            if (tool.layer.Equals("Water")) // Nếu là nước
+            {
+                //  logic fire outbreaks
+                Debug.Log("---Manh--- Check: Fire is interacting with water");
+                FinishTraining(false, "Failure! Pouring water on an electricity fire caused a dangerous flare-up.!");
+            }
+        }
+
+        protected override void HandleTimeOut()
+        {
+            FinishTraining(false, "It took too long, and the fire spread throughout the entire kitchen.!");
+        }
+    }
+}
